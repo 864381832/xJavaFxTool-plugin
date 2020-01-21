adb2214fa1658fe27f96c178796ff6ce13612e85
XUFENG-PC\xufeng <1277032935@qq.com>
2020-01-21 17:26:38 +0800

    添加小工具类插件

---
+++ littleTools/x-ImageAnalysisTool/src/main/java/com/xwintop/xJavaFxTool/Main.java
@@ -0,0 +1,49 @@
+package com.xwintop.xJavaFxTool;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Slf4j
+public class Main extends Application {
+    public static void main(String[] args) {
+        try {
+            launch(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fXMLLoader = Main.getFXMLLoader();
+        ResourceBundle resourceBundle = fXMLLoader.getResources();
+        Parent root = fXMLLoader.load();
+        primaryStage.setResizable(true);
+        primaryStage.setTitle(resourceBundle.getString("Title"));
+//        primaryStage.getIcons().add(new Image("/images/icon.jpg"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static FXMLLoader getFXMLLoader() {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("locale.ImageAnalysisTool");
+        URL url = Object.class.getResource("/com/xwintop/xJavaFxTool/fxmlView/littleTools/ImageAnalysisTool.fxml");
+        FXMLLoader fXMLLoader = new FXMLLoader(url, resourceBundle);
+        return fXMLLoader;
+    }
+}
